6e3e3a22fdbd40b5e4d9c5e88d39a0fdde69bf78
alaminusa <aamin1103@gmail.com>
2023-10-04 21:52:42 +0600

    first commit

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,9 +1,35 @@
+
 // tailwind.config =  {
 //     theme: {
 //       extend: {
+//         // my code will start here
         
+//         // all color part start
+        
+//         // all color part end
+        
+//         // all container start
+//         maxWidth: {
+//           headerContainer: '1170px',
+//         },
+//         // all container end
 
+//         // Font Family start
+//         fontFamily: {
+//           open: ["Poppins", "sans-serif"],
+//         },
+//         // Font Family end
+
+//         // Font Family start
+//         backgroundImage: {
+//           bannerImage: "url('image/banner.jpg')",
+//         },
+//         // Font Family end
+
+//         // Font size start
+//         // Font size end
         
+//         // my code will end here
 //       },
 //     },
 //   };
